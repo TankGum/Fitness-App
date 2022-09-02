78af0f90d39e13bdf81250ed9b82972295ecf1c0
tankgum <aspirineqwe01@gmail.com>
2022-09-03 01:43:26 +0700

    done video exercises

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,14 +5,18 @@ export const exerciseOptions = {
           'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
         }
     };
+
+export const youtubeOptions = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '8ebc13828dmshf8027592bd0212fp1af296jsn1f1643998a0d',
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+      }
+    };
+    
 export const fetchData = async (url, options) => {
       const res = await fetch(url, options);
       const data = await res.json();
     
       return data;
     };
-
-fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
